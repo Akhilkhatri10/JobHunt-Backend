1b153b80ac82baa684b0126f736256bb9345c067
Akhilesh Khatri <122369116+Akhilkhatri10@users.noreply.github.com>
2025-06-27 19:13:54 +0530

    Update origin for deployment

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ app.use(cookieParser())
 
 const corsOptions = {
     // origin: 'http://localhost:5173',
-    origin: 'https://job-hunt-frontend-five.vercel.app',
+    // origin: 'https://job-hunt-frontend-five.vercel.app',
+     origin: [
+    "http://localhost:5173",
+    "https://job-hunt-frontend-five.vercel.app"
+  ],
     credentials: true
 }
 app.use(cors(corsOptions))
